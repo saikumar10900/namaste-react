625279f01673a8f2c9b94bc563b5230a1b23cf5e
Saikumar <saikumar@Saikumars-MacBook-Pro.local>
2024-04-24 19:54:07 +0530

    episode 1 - day 1

---
+++ App.js
@@ -0,0 +1,17 @@
+const parentEle = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "H1 from parent"),
+    React.createElement("h2", {}, "H2 from parent"),
+  ]),
+  React.createElement("div", {}, [
+    React.createElement("h1", {}, "H1 from parent"),
+    React.createElement("h2", {}, "H2 from parent"),
+  ]),
+]);
+const heading = React.createElement(
+  "h1",
+  { id: "head" },
+  "Hello World from React"
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parentEle);
