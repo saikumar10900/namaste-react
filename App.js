37e1d03746f6e0b5bbbda390ce9b26f041a335ee
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-04-26 20:40:59 +0530

    day3 - episode2

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parentEle = React.createElement("div", { id: "parent" }, [
   React.createElement("div", {}, [
     React.createElement("h1", {}, "H1 from parent"),
