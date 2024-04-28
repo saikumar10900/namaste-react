3b00c8c1efb85f55d902986148d2bc83048fbb93
Saikumar Bolisetti <bolisettisaikumar6@gmail.com>
2024-04-28 18:25:23 +0530

    day4 - episode4

--- App.js
+++ App.js
@@ -1,19 +1,77 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const Title = () => <h1>Welcome Functional Component</h1>;
+const restarants = [
+  { name: "A" },
+  { name: "B" },
+  { name: "C" },
+  { name: "D" },
+  { name: "E" },
+];
 
-const HeaderComponent = () => {
+const Header = () => {
   return (
-    <div id="parent">
-      {Title()}
-      <Title />
-      <Title></Title>
-      <h2>Functional component is a function that returns a JSX element</h2>
-      <h3>{"we can write any JS/JSX code inside {} braces"}</h3>
+    <div className="header-container">
+      <div className="logo-container">
+        <img
+          src="https://1000logos.net/wp-content/uploads/2021/05/Swiggy-logo.png"
+          alt="logo"
+          className="app-logo"
+        />
+      </div>
+      <div className="header-children">
+        <ul className="ul-elements">
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+          <li>Home</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = (props) => {
+  const { name } = props.restarant;
+  return (
+    <div className="res-card">
+      <h1>{name}</h1>
+    </div>
+  );
+};
+
+const SearchComponent = () => {
+  return (
+    <div className="search-bar">
+      <input type="search" />
+      <button>Search</button>
+    </div>
+  );
+};
+
+const RestaurantsComponent = () => {
+  return (
+    <div className="restarntas">
+      {restarants.map((each) => (
+        <RestaurantCard restarant={each} />
+      ))}
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="app-container">
+      <div>
+        <Header />
+        <SearchComponent />
+        <RestaurantsComponent />
+      </div>
     </div>
   );
 };
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<HeaderComponent />);
+root.render(<App />);
